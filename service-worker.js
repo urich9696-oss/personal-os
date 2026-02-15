5387abf2230d3face217eba7c8cdf67ebf632af9
urich9696-oss <urich9696@gmail.com>
2026-02-15 18:53:17 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,32 @@
-const CACHE_NAME = "personal-os-v1";
+const CACHE_NAME = "personal-os-v2";
+
 const URLS = [
   "/",
   "/index.html",
   "/css/styles.css",
+  "/manifest.webmanifest",
+
   "/js/app.js",
-  "/manifest.webmanifest"
+  "/js/db.js",
+  "/js/ui.js",
+
+  "/js/screens/mindset.js",
+  "/js/screens/path.js",
+  "/js/screens/maintenance.js",
+  "/js/screens/finance.js"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
   );
 });
 
