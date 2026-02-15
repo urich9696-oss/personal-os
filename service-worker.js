4b487a2f4dc87a938eafd004826a6120ec3c895f
urich9696-oss <urich9696@gmail.com>
2026-02-15 16:46:40 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "personal-os-v1";
+const URLS = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/app.js",
+  "/manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
