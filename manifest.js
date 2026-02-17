5fb3caf338d7ac74b0804584fe839ded782de034
urich9696-oss <urich9696@gmail.com>
2026-02-17 20:48:16 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,19 +1,9 @@
 {
   "short_name": "PersonalOS",
-  "name": "PERSONAL OS: Architecture of Excellence",
-  "icons": [
-    {
-      "src": "https://cdn-icons-png.flaticon.com/512/6711/6711613.png",
-      "type": "image/png",
-      "sizes": "512x512",
-      "purpose": "any maskable"
-    }
-  ],
-  "start_url": "./index.html",
-  "background_color": "#F9F9F7",
+  "name": "PERSONAL OS",
   "display": "standalone",
-  "scope": "./",
+  "start_url": "./index.html",
   "theme_color": "#F9F9F7",
-  "description": "The Architecture of Excellence. Private Performance OS.",
-  "orientation": "portrait"
+  "background_color": "#F9F9F7",
+  "icons": [{ "src": "https://cdn-icons-png.flaticon.com/512/6711/6711613.png", "sizes": "512x512", "type": "image/png" }]
 }
